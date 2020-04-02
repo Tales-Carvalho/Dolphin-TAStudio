80bff07b621264fe0181de66a62be2cd538acdb3
sowens99 <seanowens@comcast.net>
2020-04-02 14:53:35 -0400

    Initial commit

---
+++ Dolphin TAStudio/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Dolphin_TAStudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newCtrlNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputView = new System.Windows.Forms.DataGridView();
+            this.playbackBox = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.inputView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(851, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newCtrlNToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newCtrlNToolStripMenuItem
+            // 
+            this.newCtrlNToolStripMenuItem.Name = "newCtrlNToolStripMenuItem";
+            this.newCtrlNToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.newCtrlNToolStripMenuItem.Text = "New (Ctrl + N)";
+            this.newCtrlNToolStripMenuItem.Click += new System.EventHandler(this.newCtrlNToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.openToolStripMenuItem.Text = "Open (Ctrl + O)";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.saveToolStripMenuItem.Text = "Save (Ctrl + S)";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As (Ctrl + Shift + S)";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.undoToolStripMenuItem,
+            this.redoToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.copyToolStripMenuItem.Text = "Copy (Ctrl + C)";
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.pasteToolStripMenuItem.Text = "Paste (Ctrl + V)";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteCtrlVToolStripMenuItem_Click);
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.undoToolStripMenuItem.Text = "Undo (Ctrl + Z)";
+            // 
+            // redoToolStripMenuItem
+            // 
+            this.redoToolStripMenuItem.Name = "redoToolStripMenuItem";
+            this.redoToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.redoToolStripMenuItem.Text = "Redo (Ctrl + Y)";
+            // 
+            // inputView
+            // 
+            this.inputView.AllowUserToResizeColumns = false;
+            this.inputView.AllowUserToResizeRows = false;
+            this.inputView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inputView.Location = new System.Drawing.Point(12, 27);
+            this.inputView.Name = "inputView";
+            this.inputView.Size = new System.Drawing.Size(613, 563);
+            this.inputView.TabIndex = 1;
+            this.inputView.VirtualMode = true;
+            this.inputView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.inputView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.inputView_MouseClick);
+            // 
+            // playbackBox
+            // 
+            this.playbackBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.playbackBox.Location = new System.Drawing.Point(639, 27);
+            this.playbackBox.Name = "playbackBox";
+            this.playbackBox.Size = new System.Drawing.Size(200, 100);
+            this.playbackBox.TabIndex = 2;
+            this.playbackBox.TabStop = false;
+            this.playbackBox.Text = "Playback";
+            this.playbackBox.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 602);
+            this.Controls.Add(this.playbackBox);
+            this.Controls.Add(this.inputView);
+            this.Controls.Add(this.menuStrip1);
+            this.MinimumSize = new System.Drawing.Size(867, 39);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.inputView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.DataGridView inputView;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redoToolStripMenuItem;
+        private System.Windows.Forms.GroupBox playbackBox;
+        private System.Windows.Forms.ToolStripMenuItem newCtrlNToolStripMenuItem;
+    }
+}
+
---
+++ Dolphin TAStudio/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Dolphin_TAStudio
+{
+    public partial class Form1 : Form
+    {
+        private DataTable table = new DataTable();
+        private string fileName;
+        private bool changed = false;
+        private int frameCount;
+
+        // Represent columns in a list of tuples, to allow for easy modification should these column names change
+        private List<(string, string)> columnNames = new List<(string, string)>()
+        {
+            ("Frame", "Integer"),
+            ("aX", "Integer"),
+            ("aY", "Intger"),
+            ("A", "Bool"),
+            ("B", "Bool"),
+            ("X", "Bool"),
+            ("Y", "Bool"),
+            ("Z", "Bool"),
+            ("L", "Bool"),
+            ("R", "Bool"),
+            ("La", "Integer"),
+            ("Ra", "Integer"),
+            ("dU", "Bool"),
+            ("dD", "Bool"),
+            ("dL", "Bool"),
+            ("dR", "Bool"),
+            ("cX", "Integer"),
+            ("cY", "Integer")
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            disableMenuButtons();
+        }
+
+        private void disableMenuButtons()
+        {
+            copyToolStripMenuItem.Enabled = false;
+            pasteToolStripMenuItem.Enabled = false;
+            saveToolStripMenuItem.Enabled = false;
+            saveAsToolStripMenuItem.Enabled = false;
+            undoToolStripMenuItem.Enabled = false;
+            redoToolStripMenuItem.Enabled = false;
+        }
+
+        private void openData(string fileLocation)
+        {
+            // Read from file and assert that it is formatted properly
+            using (StreamReader reader = new StreamReader(fileLocation))
+            {
+                string data = reader.ReadLine();
+                frameCount = 0;
+
+                while (!reader.EndOfStream)
+                {
+                    // TODO: Implement parser
+                }
+            }
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO: Implement save before closing
+            // if (changed)....
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                clearDataTable();
+                fileName = openFileDialog1.FileName;
+                openData(fileName);
+            }
+        }
+
+        private void clearDataTable()
+        {
+            table.Columns.Clear();
+            table.Clear();
+            undoToolStripMenuItem.Enabled = false;
+            fileName = null;
+            changed = false;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void pasteCtrlVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableGenerateColumns()
+        {
+
+        }
+
+        private void newCtrlNToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO: Save before quitting an open table
+
+            // Generate a blank table for a new recording file
+            table.Columns.Add("Frame", typeof(int));
+            table.Columns[0].ReadOnly = true;
+            table.Columns.Add("aX", typeof(int));
+            table.Columns.Add("aY", typeof(int));
+            table.Columns.Add("A", typeof(bool));
+            table.Columns.Add("B", typeof(bool));
+            table.Columns.Add("X", typeof(bool));
+            table.Columns.Add("Y", typeof(bool));
+            table.Columns.Add("Z", typeof(bool));
+            table.Columns.Add("L", typeof(bool));
+            table.Columns.Add("R", typeof(bool));
+            table.Columns.Add("La", typeof(bool));
+            table.Columns.Add("Ra", typeof(bool));
+            table.Columns.Add("u", typeof(bool));
+            table.Columns.Add("d", typeof(bool));
+            table.Columns.Add("l", typeof(bool));
+            table.Columns.Add("r", typeof(bool));
+            table.Columns.Add("cX", typeof(int));
+            table.Columns.Add("cY", typeof(int));
+            table.Rows.Add(1, 128, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 128);
+
+            inputView.DataSource = table;
+            inputView.Columns[0].Width = 60;
+            inputView.Columns[1].Width = 30;
+            inputView.Columns[2].Width = 30;
+            inputView.Columns[3].Width = 30;
+            inputView.Columns[4].Width = 30;
+            inputView.Columns[5].Width = 30;
+            inputView.Columns[6].Width = 30;
+            inputView.Columns[7].Width = 30;
+            inputView.Columns[8].Width = 30;
+            inputView.Columns[9].Width = 30;
+            inputView.Columns[10].Width = 30;
+            inputView.Columns[11].Width = 30;
+            inputView.Columns[12].Width = 30;
+            inputView.Columns[13].Width = 30;
+            inputView.Columns[14].Width = 30;
+            inputView.Columns[15].Width = 30;
+            inputView.Columns[16].Width = 30;
+            inputView.Columns[17].Width = 30;
+        }
+
+        private void inputView_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                ContextMenu menu = new ContextMenu();
+                menu.MenuItems.Add(new MenuItem("Cut Frame"));
+                menu.MenuItems.Add(new MenuItem("Copy Frame"));
+                menu.MenuItems.Add(new MenuItem("Paste Frame"));
+
+                int currentMouseOverRow = inputView.HitTest(e.X, e.Y).RowIndex;
+
+                menu.Show(inputView, new Point(e.X, e.Y));
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (inputView.SelectedRows.Count == 0) return;
+
+                // In case multiple rows are selected, just focus on last frame
+                int lastIndex = inputView.SelectedRows[inputView.SelectedRows.Count - 1].Index;
+
+                DataRow newRow;
+                newRow = table.NewRow();
+                newRow[0] = lastIndex + 2;
+                
+                for (int i = 1; i < table.Columns.Count; i++)
+                {
+                    newRow[i] = 0;
+                }
+                //newRow.ItemArray = [lastIndex + 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+                //table.Rows.InsertAt(, lastIndex + 1);
+            }
+        }
+    }
+}
